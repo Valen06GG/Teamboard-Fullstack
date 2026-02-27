8fcbfb0c3b4f9bd1b6b312821398125c5f4cf4df
Valentin Fortunato <valenfortunato06@gmail.com>
2026-02-27 12:31:37 -0300

    3er Commit | Project-Teamboard-Fullstack

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { ProjectsService } from './projects.service';
 
 @Controller('projects')
+@UseGuards(JwtAuthGuard)
 export class ProjectsController {
+  constructor(private readonly projectsService: ProjectsService) {}
+  
+  @Post()
+  create(
+    @Body() body: { name: string, description: string },
+    @Req() req,
+  ) {
+    return this.projectsService.create(body, req.user.companyId);
+  }
 
-  @Get('test')
-  @UseGuards(JwtAuthGuard)
-    test(@Req() req) {
-      return req.user;
+  @Get()
+  findAll(@Req() req) {
+    return this.projectsService.findAll(req.user.companyId);
   }
 }
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './projects.entity';
+import { Repository } from 'typeorm';
+import { Company } from 'src/companies/companies.entity';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+    constructor(
+        @InjectRepository(Project)
+        private projectsRepository: Repository<Project>,
+    ) {}
+
+  async create(data: any, companyId: string) {
+    const project = this.projectsRepository.create({
+        ...data,
+        company: { id: companyId } as Company,
+    });
+
+    return this.projectsRepository.save(project);
+  }
+  
+  async findAll(companyId: string) {
+    return this.projectsRepository.find({
+        where: {
+            company: { id: companyId },
+        },
+    });
+  }
+}
