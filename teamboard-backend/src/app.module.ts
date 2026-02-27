812226cec9212a27d514c6b5283c5c10d581d86a
Valentin Fortunato <valenfortunato06@gmail.com>
2026-02-27 12:37:46 -0300

    4to Commit | Project-Teamboard-Fullstack

--- teamboard-backend/src/app.module.ts
+++ teamboard-backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CompaniesModule } from './companies/companies.module';
-import { UsersModule } from './users/users.module';
-import { TasksModule } from './tasks/tasks.module';
-import { ProjectsModule } from './projects/projects.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { CompaniesModule } from 'src/companies/companies.module';
+import { ProjectsModule } from 'src/projects/projects.module';
+import { TasksModule } from 'src/tasks/tasks.module';
+import { UsersModule } from 'src/users/users.module';
 
 
 @Module({
