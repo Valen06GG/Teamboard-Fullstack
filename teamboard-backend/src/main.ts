4990b64d9d055b990c8a44d1e9981c75d46d79a0
Valentin Fortunato <valenfortunato06@gmail.com>
2026-03-20 10:05:22 -0300

    fix cors

--- teamboard-backend/src/main.ts
+++ teamboard-backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      'https://teamboard-frontend-eta.vercel.app'],
+      'https://teamboard-frontend-o5wvb5bcy-valentins-projects-0e36b93a.vercel.app'],
     credentials: true,
   });
 
